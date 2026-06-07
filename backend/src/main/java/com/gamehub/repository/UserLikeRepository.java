bd9cd8f219265bd30cbb7af3ded7153f6f396499
wx <193874529@qq.com>
2026-06-07 19:44:54 +0800

    清理多余import

--- backend/src/main/java/com/gamehub/repository/UserLikeRepository.java
+++ backend/src/main/java/com/gamehub/repository/UserLikeRepository.java
@@ -2,8 +2,6 @@ package com.gamehub.repository;
 
 import com.gamehub.entity.UserLike;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface UserLikeRepository extends JpaRepository<UserLike, Long> {
     boolean existsByUserIdAndVideoId(Long userId, Long videoId);
