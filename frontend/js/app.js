0dba370a4b924a039af5ca652790f3f787d361e6
wx <193874529@qq.com>
2026-06-07 16:38:09 +0800

    清理LOGO选择功能残留代码

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1871,22 +1871,9 @@
         document.body.classList.add('theme-' + currentTheme);
     }
 
-    // Apply chosen logo from localStorage
-    function applyStoredLogo() {
-        var chosen = localStorage.getItem('bobi_logo');
-        if (!chosen || chosen === 'logo.svg') return;
-        var src = 'img/' + chosen;
-        document.querySelectorAll('img[src*="logo.svg"]').forEach(function(el) {
-            el.src = src;
-        });
-        var fav = document.querySelector('link[rel="icon"]');
-        if (fav && chosen === 'logo7.svg') fav.href = 'img/favicon.svg';
-    }
-
     // ============ INIT ============
     function init() {
         bindEvents();
-        applyStoredLogo();
 
         if (AUTH.isLoggedIn()) {
             onLoginSuccess();
